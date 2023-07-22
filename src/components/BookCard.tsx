9c0df0ebcf11934507dc69ba1506efd70234f77d
Arman-Kabir <armankabir8@gmail.com>
2023-07-22 15:49:30 +0600

    bookCard done

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,8 +1,28 @@
+import { IBook } from "@/types/globalTypes";
 
-function BookCard() {
+interface IProps {
+    book: IBook;
+}
+
+function BookCard({ book }: IProps) {
+    console.log(book);
     return (
         <div>
-            BookCard
+            {/* flex flex-col items-center */}
+            <div className="p-5">
+                
+                <div className="">
+                {/* bookCard-image */}
+                    <img className="h-96 w-full" src={book.image} alt="" />
+                </div>
+                <h3 className="text-2xl font-bold text-center text-purple-600 pt-3">{book.title}</h3>
+                <p className="text-purple-400 text-center text-xl">Written By : {book.author}</p>
+
+                <div className="flex justify-around text-xl text-purple-400">                    
+                    <p className="">{book.genre}</p>
+                    <p className="">{book.publication_date}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import BookCard from "@/components/BookCard";
 import { IBook } from "@/types/globalTypes";
 import { useEffect, useState } from "react";
@@ -7,6 +8,7 @@ function Home() {
     console.log(data);
     let booksData;
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     booksData=data?.data;
     
 
@@ -20,9 +22,9 @@ function Home() {
     },[]);
 
     return (
-        <div className="w-9/12 mx-auto bg-slate-500">
-            <h2 className="p-5">Recently Added Books</h2>
-            <div className="">
+        <div className="w-9/12 mx-auto">
+            <h2 className="p-5 text-center text-4xl text-purple-600 font-black">Recently Added Books</h2>
+            <div className="grid grid-cols-4 gap-5 pt-6">
                 {booksData?.map((book:IBook)=>(
                     <BookCard  key = {book._id} book={book}></BookCard>
                 ))}
