5996a6617de4a49129e544c1928c977b2c188353
Arman-Kabir <armankabir8@gmail.com>
2023-09-19 15:03:20 +0600

    store setup and apiSlice done

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,15 +1,19 @@
 import { IBook } from "@/types/globalTypes";
 import SingleBook from "../components/SingleBook";
+import { useGetBooksQuery } from "@/redux/api/apiSlice";
 
-function AllBooks({ books }) {
+function AllBooks() {
+  const {data,isLoading,error} = useGetBooksQuery(undefined);
   // console.log(books);
+  console.log(data);
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 pt-6">
-        {books?.map((book: IBook) => (
+        ALl Books
+        {/* {books?.map((book: IBook) => (
           // console.log(book);
           <SingleBook book={book} key={book._id}></SingleBook>
-        ))}
+        ))} */}
       </div>
     </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,28 +5,28 @@ import { IBook } from "@/types/globalTypes";
 import { useEffect, useState } from "react";
 
 function Home() {
-    const [data, setData] = useState<IBook[]>([]);
+    // const [data, setData] = useState<IBook[]>([]);
     // console.log(data);
-    const booksData = data?.data;
+    // const booksData = data?.data;
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     
     // console.log(booksData);
 
 
-    useEffect(() => {
-        fetch('http://localhost:5001/api/v1/books')
-            .then((res) => res.json())
-            .then((data: IBook[]) => setData(data))
-            .catch((error) => {
-                console.log('Error fetching data', error)
-            })
-    }, []);
+    // useEffect(() => {
+    //     fetch('http://localhost:5001/api/v1/books')
+    //         .then((res) => res.json())
+    //         .then((data: IBook[]) => setData(data))
+    //         .catch((error) => {
+    //             console.log('Error fetching data', error)
+    //         })
+    // }, []);
 
     return (
         <div className="w-9/12 mx-auto">
             <h2 className="p-5 text-center text-4xl text-purple-600 font-black">Recently Added Books</h2>
-            <AllBooks books={booksData}></AllBooks>
+            {/* <AllBooks books={booksData}></AllBooks> */}
         </div>
     )
 }
