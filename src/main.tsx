4cc507094f6f1502ffe4b764eca9876b221c50de
Arman-Kabir <armankabir8@gmail.com>
2023-10-01 02:22:30 +0600

    redus-persist added

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,20 @@ import { RouterProvider } from 'react-router-dom';
 import './index.css'
 import routes from './routes/routes';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import { ToastContainer, toast } from 'react-toastify';
+import { store, persistor } from './redux/store';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+
+// let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={routes} />
+      <PersistGate persistor={persistor}>
+        <RouterProvider router={routes} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { api } from './api/apiSlice';
 import listSlice from './features/wishlist/listSlice';
 import readingSlice from './features/reading/readingSlice';
 
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const rootReducer = combineReducers({
+    list: listSlice,
+    reading: readingSlice,
+    [api.reducerPath]: api.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-    reducer: {
-        list: listSlice,
-        reading: readingSlice,
-        [api.reducerPath]: api.reducer
-    },
+    reducer: persistedReducer,
+    // {
+    //     // list: listSlice,
+    //     // reading: readingSlice,
+    //     // [api.reducerPath]: api.reducer
+    // },
 
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(api.middleware),
@@ -18,5 +35,6 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+const persistor = persistStore(store);
 
-export default store;
+export  { store, persistor };
