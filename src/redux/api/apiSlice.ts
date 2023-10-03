7e65c60d5bd7c4c23fffe2283ab26ec2d0e58612
Arman-Kabir <armankabir8@gmail.com>
2023-10-03 17:40:45 +0600

    render url added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,24 +2,25 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5009' }),
+    // http://localhost:5009
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://booknet-backend-simple.onrender.com' }),
     endpoints: (builder) => ({
         getBooks: builder.query({
             query: () => '/books',
-            providesTags: ['deletion','unique_tag_for_getBooks'],
+            providesTags: ['deletion', 'unique_tag_for_getBooks'],
         }),
         getSingleBook: builder.query({
             query: (id: string) => `/books/${id}`,
             providesTags: ['comments'],
         }),
         postComment: builder.mutation({
-            query: ( {id, data} ) => {
+            query: ({ id, data }) => {
                 // Log the 'data' to check if it's passed or not
                 console.log('Data:', data);
                 return {
                     url: `/comment/${id}`,
                     method: "PATCH",
-                    body:data
+                    body: data
                 };
             },
             invalidatesTags: ['comments'],
@@ -49,4 +50,4 @@ export const api = createApi({
     })
 });
 
-export const { useGetBooksQuery, useGetSingleBookQuery, usePostBookMutation, useEditBookMutation, usePostCommentMutation,useDeleteBookMutation } = api;
+export const { useGetBooksQuery, useGetSingleBookQuery, usePostBookMutation, useEditBookMutation, usePostCommentMutation, useDeleteBookMutation } = api;
