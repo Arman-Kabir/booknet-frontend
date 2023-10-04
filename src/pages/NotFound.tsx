da85b63896c1c23f6488ddab13b8fc7ba8a21bbf
Arman-Kabir <armankabir8@gmail.com>
2023-10-05 00:32:25 +0600

    navigation changed at not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom"
 
 const NotFound = () => {
     const navigate = useNavigate();
-    navigate(-1);
+    navigate('/all-books');
     return (
         <div>Not Found ...</div>
     )
